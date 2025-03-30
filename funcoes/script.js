950505fde69972f9311a541913c994ce4549712b
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-03-30 13:19:10 -0300

    Finaliza a aula 2 de Funções

--- funcoes/script.js
+++ funcoes/script.js
@@ -59,3 +59,76 @@ addEventListener('click', mostraConsole);
 
 // FUNÇÕES
 
+//Pode ou não retornar um valor
+//Quando não definimos o return, ela irá retornar undefined. O código interno da função é executado normalmente, independente de existir valor de return ou não.
+function imc2(peso, altura) {
+  const imc2 = peso / (altura ** 2);
+  console.log(imc2);
+}
+
+imc2(80, 1.80); // retorna o imc
+console.log(imc2(80, 1.80)); // retorna o imc e undefined
+
+// Valores retornados
+// Uma função pode retornar qualquer tipo de dado e até outras funções
+function terceiraIdade(idade) {
+  if(typeof idade !== 'number') {
+    return 'Informe a sua idade!';
+  } else if(idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Retornar diferentes tipos de dados na mesma função não é uma boa ideia
+
+function primeiraIdade(idade1) {
+  console.log(typeof idade1);
+  if(typeof idade1 !== 'number') {
+    return 'Por favor, preencha um número'
+  } else if(idade1 >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+  console.log(idade1);
+}
+
+console.log(primeiraIdade(30));
+
+// ESCOPO
+//Variáveis e funções definidas dentro de um bloco {}, não são visíveis fora dele.
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} paises para visitar`
+}
+console.log(totalPaises); //erro, totalPaises não definido
+
+//variavel criada fora da função consigo ver dentro, mas criada dentro não consigo ver fora
+
+var profissao = 'Designer';
+
+function dados() {
+  var nome = 'André';
+  var idade = 28;
+  function outrosDados() {
+    var endereco = 'Rio de Janeiro';
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return outrosDados();
+}
+
+dados(); // Retorna 'André, 29, Rio de Janeiro, Designer'
+outrosDados(); // retorna um erro
+
+// HOISTING
+//Antes de executar uma função, o JS 'move' todas as funções declaradas para a memória
+
+imc(80, 1.80); // imc aparece no console
+
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2);
+  console.log(imc);
+}
