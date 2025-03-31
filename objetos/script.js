87eb3d198a14b77749f255a525fcb865ce809dcc
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-03-31 20:38:52 -0300

    Finaliza a aula 1 de Objetos

--- objetos/script.js
+++ objetos/script.js
@@ -14,8 +14,8 @@ pessoa.possuiFaculdade; // true
 //MÉTODOS
 //é uma propriedade que possui uma função no local do seu valor
 var quadrado = {
-  lados: 4,
-  area: function(lado) {
+  lados: 4, //propriedade
+  area: function(lado) { //método
     return lado * lado;
   },
   perimetro: function(lado) {
@@ -26,3 +26,23 @@ var quadrado = {
 quadrado.lados; // 4
 quadrado.area(5); // 25
 quadrado.perimetro(5); // 20
+
+//Abreviação de area: function() {} para area() {}, no ES6+
+
+var quadrado = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    return this.lados * lado;
+  },
+}
+
+
+// UM OBJETO SERVE PARA ORGANIZAR O CÓDIGO
+Math.PI; // 3.14
+Math.random(); // número aleatório, quebrado, de zero a um
+
+var pi = Math.PI;
+console.log(pi); // 3.14
