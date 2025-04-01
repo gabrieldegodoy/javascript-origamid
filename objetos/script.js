d2cb704f5bf4e32b90f556325af0a2c6a138de34
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-04-01 20:38:10 -0300

    Finaliza a aula 2 de Objetos

--- objetos/script.js
+++ objetos/script.js
@@ -55,3 +55,107 @@ var carro = {};
 var pessoa = {};
 
 console.log(typeof carro); // 'object'
+
+//Dot Notation Get
+//Acesse propriedades de um objeto utilizando o ponto .
+
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+}
+
+var bg = menu.backgroundColor; // '#84E'
+
+//Dot Notation Set
+//Substitua o valor de uma propriedade utilizando . e o = após o nome da mesma.
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+}
+
+menu.backgroundColor = '#000';
+console.log(menu.backgroundColor); // '#000'
+
+//Adicionar Propriedades e Métodos
+//Basta adicionar um novo nome e definir o valor.
+var menu = {
+  width: 800,
+}
+
+menu.height = 50;
+menu.position = 'fixed';
+
+//Palavra-chave this
+//this irá fazer uma referência ao próprio objeto.
+var height = 120;
+var menu = {
+  width: 800,
+  height: 50,
+  metadeHeight() {
+    return this.height / 2;
+  }
+}
+
+menu.metadeHeight(); // 25
+// sem o this, seria 60
+
+//Protótipo e Herança
+//O objeto herda propriedades e métodos do objeto que foi utilizado para criar o mesmo.
+var menu = {
+  width: 800,
+}
+
+menu.hasOwnProperty('width') // true
+menu.hasOwnProperty('height') // false
+
+//Exercício
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+// Crie um método no objeto, que mostre o seu nome completo
+
+var dadosPessoais = {
+  nome: 'Gabriel',
+  sobrenome: 'Godoy',
+  idade: 23,
+  carro: 'Mustang',
+  nomeCompleto() {
+    return `${this.nome} ${this.sobrenome}`;
+  }
+};
+
+console.log(dadosPessoais);
+console.log(dadosPessoais.nomeCompleto());
+
+
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+console.log(carro);
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+  raca: 'Labrador',
+  cor: 'Preto',
+  idade: 10,
+  latir(pessoa) {
+    if(pessoa === 'homem') {
+      return 'Latiu!';
+    } else {
+      return 'Nada';
+    }
+  }
+}
+
+console.log(cachorro);
+console.log(cachorro.latir('homem'));
