5e8dd2f3cb95295c22fe59ec8779d31127588ba1
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-04-01 21:30:55 -0300

    Finaliza a aula final introdutória de Objetos

--- objetos/script.js
+++ objetos/script.js
@@ -161,4 +161,79 @@ console.log(cachorro);
 console.log(cachorro.latir('homem'));
 
 // TUDO É OBJETO
+//Tudo é Objeto
+//Strings, Números, Boolean, Objetos e mais, possuem propriedades e métodos. Por isso são objetos.
+var nome = 'André';
 
+nome.length; // 5
+nome.charAt(1); // 'n'
+nome.replace('ré', 'rei'); // 'Andrei'
+nome; // 'André'
+
+//Uma string herda propriedades e métodos do construtor String()
+
+//Números
+var altura = 1.8;
+
+altura.toString(); // '1.8'
+altura.toFixed(); // '2'
+
+//Por um breve momento o número é envolvido em um Objeto (coerção), tendo acesso assim as suas propriedades e métodos
+
+
+//Funções
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+areaQuadrado.toString();
+//"function areaQuadrado(lado) {
+//  return lado * lado;
+//}"
+
+areaQuadrado.length; // 1
+
+
+//Elementos do DOM
+/*var btn = document.querySelector('.btn');
+
+btn.classList.add('azul') // adiciona a classe azul
+btn.innerText; // 'Clique'
+btn.addEventListener('click', function() {
+  console.log('Clicou')
+})*/
+
+//Praticamente todos os efeitos com JS são feitos utilizando propriedades e métodos de objetos do DOM.
+
+
+//EXERCÍCIO
+// nomeie 3 propriedades ou métodos de strings
+
+var nome1 = 'Gabriel';
+
+console.log(nome1.length, nome1.charAt(3), nome1.repeat(3));
+
+
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn');
+
+function addClass() {
+  return btn.classList.add('Azul');
+}
+
+btn.addEventListener('Click', addClass());
+
+console.log(btn.lastChild);
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+//var texto = 'Olá, mundo!';
+
+//navigator.clipboard.writeText(texto)
+//navigator.clipboard.readText()
+
+//nesse caso, procurar métodos e apis para as minhas necessidades, seguir instrução de instalação/forma de aplicar
