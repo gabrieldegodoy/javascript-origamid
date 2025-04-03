6de7549d6a4620b71f37b1ba88516f72e55a31f0
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-04-03 20:32:33 -0300

    Finaliza aula 1 de dom

--- o-que-e-dom/script.js
+++ o-que-e-dom/script.js
@@ -1,1 +1,16 @@
 // DOM
+//Document Object Model (DOM)
+//É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
+console.log(window);
+// window é o objeto global do browser
+// possui diferentes métodos e propriedades
+
+window.innerHeight; // retorna a altura do browser
+//Ao inspecionar elemento com o Chrome, você está vendo a representação oficial do DOM.
+
+//window.alert('isso mesmo');
+const href = window.location.href;
+console.log(href);
+
+const h1Selecionado = document.querySelector('h1'); //seleciona o primeiro h1
+document.body; //retorna o body
