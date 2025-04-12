5cf9be65e7c6dd97bc6949f0e89e78696506ce2e
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-04-12 14:27:51 -0300

    Finaliza a aula de Animação ao Scroll

--- animacao-ao-scroll/script.js
+++ animacao-ao-scroll/script.js
@@ -82,6 +82,26 @@ function initScrollSuave() {
 initScrollSuave();
 
 // ANIMAÇÃO AO SCROLL
+function initAnimaScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
 
+  if(sections.length) {
+    const windowMetade =window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const inSectionVisible = (sectionTop - windowMetade) < 0;
+        if(inSectionVisible) {
+          section.classList.add('ativo');
+        }
+      });
+    }
+    animaScroll()
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimaScroll();
 
 
