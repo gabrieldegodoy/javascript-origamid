8df561efee988f6c188c5da883ec56142d75a1b8
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-04-12 11:40:42 -0300

    Finaliza a aula de Scroll Suave

--- scroll-suave-link-interno/script.js
+++ scroll-suave-link-interno/script.js
@@ -52,4 +52,33 @@ function initAccordion() {
 }
 initAccordion();
 
+// SCROLL SUAVE LINK INTERNO
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  if(linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      
+      //Forma Alternativa
+      /*const topo = section.offsetTop;
+      window.scrollTo({
+        top: topo,
+        behavior: 'smooth',
+      });*/
+
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+}
+initScrollSuave();
+
 
