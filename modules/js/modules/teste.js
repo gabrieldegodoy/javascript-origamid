17b70850dd88cb188291aa9fdcf2f1bc5650845a
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-04-18 10:53:02 -0300

    Finaliza a aula de Modules

---
+++ modules/js/modules/teste.js
@@ -0,0 +1,9 @@
+export function teste1() {
+  console.log('Isso é um teste1');
+}
+
+export function teste2() {
+  console.log('Isso é um teste2');
+}
+
+export const senha = 'ouhaewyfgq';
---
+++ modules/js/script.js
@@ -0,0 +1,13 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import * as teste from './modules/teste.js';
+
+console.log(teste.teste1());
+console.log(teste.senha);
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
