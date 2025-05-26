38b6ca958ac1b28c59b179030b34fe74acfe5859
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-05-26 14:23:58 -0300

    Finaliza a aula de Extends

--- extends/script.js
+++ extends/script.js
@@ -1,1 +1,25 @@
 //EXTENDS
+class Veiculo {
+  constructor(rodas) {
+    this.rodas = rodas;
+  }
+  acelerar() {
+    console.log('Acelerou!');
+  }
+}
+
+class Moto extends Veiculo {
+  constructor(rodas, capacete) {
+    super(rodas);
+    this.capacete = capacete; //esse this cria uma propriedade no objeto que está sendo criadopelo constructor, como o valor do parametro atribuido a ela nesse caso.
+  }
+  acelerar() {
+    super.acelerar();
+    console.log('Muito!');
+  }
+  empinar() {
+    console.log('Moto empinou com ' + this.rodas + ' rodas.');
+  }
+}
+
+const honda = new Moto(2);
