6165a2803818b4f4de1904d2df37b5bcca0e6c27
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-05-27 16:11:15 -0300

    Finaliza a aula de Closures e Debugging

--- closures-e-debugging/script.js
+++ closures-e-debugging/script.js
@@ -1,1 +1,13 @@
 // CLOSURES E DEBUGGING
+function contagem() {
+  let total = 0;
+  return function incrementar() {
+    total++;
+    console.log(total);
+  }
+}
+
+const ativarIncrementar = contagem();
+ativarIncrementar(); // 1
+ativarIncrementar(); // 2
+ativarIncrementar(); // 3
